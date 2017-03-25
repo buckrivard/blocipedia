203ca35e1a3c293f9e671cc4c3815eb667b5ecf4
Charlie Gaines <beaugaines@gmail.com>
2017-03-24 23:24:15 -0400

    various fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,36 @@
-require 'random_data'
+User.destroy_all
+Wiki.destroy_all
 
+CURRENT_TIME = Time.now
+
+def some_time_ago
+  CURRENT_TIME - rand(200..20_000).minutes
+end
+
+5.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    confirmed_at: Time.now
+  )
+end
+
+# your user
+User.create(email: 'me@blocipedia.com', password: 'password', confirmed_at: Time.now)
+
+users = User.all
 
 50.times do
-	wiki = Wiki.create!(
-		title:  RandomData.random_sentence,
-		body:   RandomData.random_paragraph,
-		private: false,
+	wiki = Wiki.create(
+		title:  "#{Faker::Hacker.noun.upcase}!",
+		body:   Faker::Hacker.say_something_smart,
+    user: users.sample
 	)
-
-	wiki.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  # update_attribute is deprecated, prefer update
+	wiki.update(created_at: some_time_ago)
 end
 
 
-
 puts "Seed finished"
+puts "#{User.count} users created."
 puts "#{Wiki.count} wikis created."
