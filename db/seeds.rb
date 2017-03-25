923020c06673a27b5855b862e12d8bfb4a5fde08
Buck Rivard <buckrivard@gmail.com>
2017-03-24 23:48:30 -0700

    db error fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,33 @@
-require 'random_data'
+User.destroy_all
+Wiki.destroy_all
 
+CURRENT_TIME = Time.now
 
-50.times do
-	wiki = Wiki.create!(
-		title:  RandomData.random_sentence,
-		body:   RandomData.random_paragraph,
-		private: false,
-	)
-
-	wiki.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+def some_time_ago
+	CURRENT_TIME - rand(200..20_000).minutes
 end
 
+5.times do
+  User.create(
+     email: Faker::Internet.email,
+     password: Faker::Internet.password,
+     confirmed_at: Time.now
+  )
+end
 
+users = User.all
+
+50.times do
+ 	wiki = Wiki.create(
+ 		title:  "#{Faker::Hacker.noun.upcase}!",
+ 		body:   Faker::Hacker.say_something_smart,
+     user: users.sample
+ 	)
+	wiki.update(created_at: some_time_ago)
+end
+
+User.create(email: 'buckrivard@gmail.com', password: 'helloworld', confirmed_at: Time.now)
 
 puts "Seed finished"
+puts "#{User.count} users created."
 puts "#{Wiki.count} wikis created."
