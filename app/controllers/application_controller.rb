f72f266bd6acf4119c0e274b9f86ae3845781a1f
Buck Rivard <buckrivard@gmail.com>
2017-03-27 16:50:33 -0700

    vertical scroll wiki list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+  	flash[:alert] = "You must be an admin to access this feature."
+  	redirect_to(request.referer)
+  end
   
 end
