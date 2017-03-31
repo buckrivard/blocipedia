9d4d97e922cad1f78780f7f64a5a685ae4494401
Buck Rivard <buckrivard@gmail.com>
2017-03-30 18:10:33 -0700

    collaborators column added to wikis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330210811) do
+ActiveRecord::Schema.define(version: 20170330220452) do
 
   create_table "collaborators", force: :cascade do |t|
     t.integer  "user_id"
@@ -52,8 +52,9 @@ ActiveRecord::Schema.define(version: 20170330210811) do
     t.text     "body"
     t.boolean  "private"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "collaborators"
   end
 
   add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
